8a07e8c413d3b0fcda2718494c8d5fde85ad19f4
wm <eclipse_sv@163.com>
2016-04-09 11:34:09 +0800

    0409

--- app/index.py
+++ app/index.py
@@ -1,14 +1,14 @@
-#-*- coding: UTF-8 -*- 
-from flask import Flask,render_template,redirect,request,url_for,flash
+#-*- coding: UTF-8 -*-
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
 from get_tags import get_tags
 from bookdata import get_bookinfo
-from flask.ext.login import LoginManager,login_user,logout_user,login_required,current_user
-from forms import LoginForm,RegistrationForm,ChangePasswordForm,\
-	PasswordRestRequestForm,PasswordResetForm,ChangeEmailForm
+from flask.ext.login import LoginManager, login_user, logout_user, login_required, current_user
+from forms import LoginForm, RegistrationForm, ChangePasswordForm,\
+    PasswordRestRequestForm, PasswordResetForm, ChangeEmailForm
 from .. import email
 from . import send_email
 
@@ -26,115 +26,121 @@ moment = Moment(app)
 login_manager.init_app(app)
 
 
-
 # Things about User
 @before_app_request
 def berfore_request():
-	if current_user.is_authenticated:
-		if not current_user.confirmed \
-			and request.endpoint[:5] != 'auth.' \
-			and request.endpoint != 'static':
-			return redirect(url_for('unconfirmed'))
+    if current_user.is_authenticated:
+        if not current_user.confirmed \
+                and request.endpoint[:5] != 'auth.' \
+                and request.endpoint != 'static':
+            return redirect(url_for('unconfirmed'))
+
 
 @app.route('/unconfirmed')
 def unconfirmed():
-	if current_user.is_anonymous or current_user.confirmed:
-		return redirect(url_for('index'))
-	return render_template('/unconfirmed.html')
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('index'))
+    return render_template('/unconfirmed.html')
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		user = User.query({'email':form.email.data})
-		if user is not None and user.varify_password(form.password.data):
-			login_user(user,form.remember_me.data)
-			return redirect(request.args.get('next') or url_for('index'))
-		flash('Invalid username or password')
-	return render_template('login.html',form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query({'email': form.email.data})
+        if user is not None and user.varify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('index'))
+        flash('Invalid username or password')
+    return render_template('login.html', form=form)
+
 
 @app.route('/logout')
 @login_required
 def logout():
-	logout_user()
-	flash('You have been logged out')
-	return redirect(url_for('index'))
+    logout_user()
+    flash('You have been logged out')
+    return redirect(url_for('index'))
 
-@app.route('/register',methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-	form = RegistrationForm()
-	if form.validate_on_submit():
-		user = User(email=form.email.data,
-					username=form.username.data,
-					password=form.password.data)
-		User.commit(user.data)
-		token = user.generate_confirmation_token()
-		send_email(user.email,'Confirm your Account','auth/email/confirm',
-			user=user,token=token)
-		flash('A confirmation email has been sent to you')
-		return redirect(url_for('login'))
-	return render_template('register.html',form=form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        User.commit(user.data)
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm your Account', 'auth/email/confirm',
+                   user=user, token=token)
+        flash('A confirmation email has been sent to you')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
 
 @app.route('/confirm/<token>')
 @login_required
 def confirm(token):
-	if current_user.confirmed:
-		return redirect(url_for('index'))
-	if current_user.confirm(token):
-		flash('You have confirmed your account. Thanks!')
-	else:
-		flash('The confirmation link is invalid or has expired.')
-	return redirect(url_for('index'))
+    if current_user.confirmed:
+        return redirect(url_for('index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('index'))
+
 
 @app.route('/confirm')
 @login_required
 def resend_confirmation():
-	token = current_user.generate_confirmation_token()
-	send_email(current_user.email,'Confirm your account','/auth/email/confirm',
-		user=current_user,token=token)
-	flash('A new confirmation email has been sent to you!')
-	return redirect(url_for('index'))
-
-
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm your account', '/auth/email/confirm',
+               user=current_user, token=token)
+    flash('A new confirmation email has been sent to you!')
+    return redirect(url_for('index'))
 
 
 @app.route('/')
 def index():
-	return render_template('index.html',current_time=datetime.utcnow())
+    return render_template('index.html', current_time=datetime.utcnow())
+
 
 @app.route('/music')
 def musictags():
-	tags = get_tags('music')
-	if tags!=[]:
-		return render_template('music.html',current_time=datetime.utcnow(),tags=tags,flag=0)
-	else:
-		return render_template('index.html',current_time=datetime.utcnow())
+    tags = get_tags('music')
+    if tags != []:
+        return render_template('music.html', current_time=datetime.utcnow(), tags=tags, flag=0)
+    else:
+        return render_template('index.html', current_time=datetime.utcnow())
+
 
 @app.route('/movie')
 def movietags():
-	tags = get_tags('movie')	
-	if tags!=[]:
-		return render_template('movie.html',current_time=datetime.utcnow(),tags=tags,flag=0)
-	else:
-		return render_template('index.html',current_time=datetime.utcnow())
+    tags = get_tags('movie')
+    if tags != []:
+        return render_template('movie.html', current_time=datetime.utcnow(), tags=tags, flag=0)
+    else:
+        return render_template('index.html', current_time=datetime.utcnow())
+
 
 @app.route('/book')
 def booktags():
-	tags = get_tags('book')	
-	if tags!=[]:
-		return render_template('book.html',current_time=datetime.utcnow(),tags=tags,flag=0)
-	else:
-		return render_template('index.html',current_time=datetime.utcnow())
+    tags = get_tags('book')
+    if tags != []:
+        return render_template('book.html', current_time=datetime.utcnow(), tags=tags, flag=0)
+    else:
+        return render_template('index.html', current_time=datetime.utcnow())
+
 
 @app.route('/bookinfo/<booktag>')
 def bookinfo(booktag):
-	bookdata = get_bookinfo(booktag)
-	if bookdata:
-		return render_template('bookinfo.html',bookdata=bookdata,flag=0)
-	else:
-		return render_template('index.html',current_time=datetime.utcnow())
-
+    bookdata = get_bookinfo(booktag)
+    if bookdata:
+        return render_template('bookinfo.html', bookdata=bookdata, flag=0)
+    else:
+        return render_template('index.html', current_time=datetime.utcnow())
 
 
 if __name__ == '__main__':
-	manager.run()
+    manager.run()
--- app/models.py
+++ app/models.py
@@ -1,121 +1,122 @@
-#-*- coding: UTF-8 -*- 
+#-*- coding: UTF-8 -*-
 import hashlib
 from datetime import datetime
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 import bleach
-from flask import current_app,request
-from flask.ext.login import UserMixin,AnonymousUserMixin
+from flask import current_app, request
+from flask.ext.login import UserMixin, AnonymousUserMixin
 from db import client
 from random import randint
 
-class User(UserMixin):
-	def __init__(self,id=None,email=None,username=None,password=None,location=None,about_me=None):
-		if id is None:
-			self.id=randint(1,10000)
-		else:
-			self.id=id
-		self.email=email
-		self.username = username
-		self.password = password
-		self.confirmed = False
-		self.location = location
-		self.about_me = about_me
-		self.client = client
-		self.col = client['flask']['users']
-		self.data = {
-					"id":self.id,
-					"email":self.email,
-					"username":self.username,
-					"password":password,
-					"confirmed":self.confirmed,
-					"location":self.location,
-					"about_me":self.about_me
-			}
 
-	@staticmethod
-	def query(data):
-		col = client['flask']['users']
-		cursor = col.find(data).limit(1)
-		for doc in cursor:
-			u =User(id=doc['id'],
-					 email=doc['email'],
+class User(UserMixin):
+
+    def __init__(self, id=None, email=None, username=None, password=None, location=None, about_me=None):
+        if id is None:
+            self.id = randint(1, 10000)
+        else:
+            self.id = id
+        self.email = email
+        self.username = username
+        self.password = password
+        self.confirmed = False
+        self.location = location
+        self.about_me = about_me
+        self.client = client
+        self.col = client['flask']['users']
+        self.data = {
+            "id": self.id,
+            "email": self.email,
+            "username": self.username,
+            "password": password,
+            "confirmed": self.confirmed,
+            "location": self.location,
+            "about_me": self.about_me
+        }
+
+    @staticmethod
+    def query(data):
+        col = client['flask']['users']
+        cursor = col.find(data).limit(1)
+        for doc in cursor:
+            u = User(id=doc['id'],
+                     email=doc['email'],
                      username=doc['username'],
                      password=doc['password'],
                      location=doc['location'],
                      about_me=doc['about_me']
                      )
-			return u 
+            return u
 
-	@staticmethod
-	def commit(data):
-		col = client['flask']['users']
-		col.insert_one(data)
+    @staticmethod
+    def commit(data):
+        col = client['flask']['users']
+        col.insert_one(data)
 
+    @staticmethod
+    def generate_fake(count=100):
+        from random import seed
+        import forgery_py
 
-	@staticmethod
-	def generate_fake(count=100):
-		from random import seed
-		import forgery_py
-
-		seed()
-		for i in range(count):
-			u =User(email=forgery_py.internet.email_address(),
+        seed()
+        for i in range(count):
+            u = User(email=forgery_py.internet.email_address(),
                      username=forgery_py.internet.user_name(True),
                      password=forgery_py.lorem_ipsum.word(),
                      location=forgery_py.address.city(),
                      about_me=forgery_py.lorem_ipsum.sentence())
-			data = u.data
-			
-			u.col.insert_one(data)
+            data = u.data
 
-	@property
-	def password(self):
-		raise AttributeError('password is not a readable attribute')
+            u.col.insert_one(data)
 
-	@password.setter
-	def password(self,password):
-		self.password_hash= generate_password_hash(password)
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
 
-	def verify_password(self,password):
-		return check_password_hash(self.password_hash,password)
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-	def generate_confirmation_token(self,expiration=3600):
-		s = Serializer(current_app.config['SECRET_KEY'],expiration)
-		return s.dumps({'confirm':self.id})
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-	def confirm(self,token):
-		s = Serializer(current_app.config['SECRET_KEY'])
-		try:
-			data = s.loads(token)
-		except:
-			return False
-		if data.get('confirm')!=self.id:
-			return False
-		self.confirm = True
-		self.col.update_one(
-			{'email',self.email},
-			{"$set":{"confirmed":self.confirmed}}
-			)
-		return True
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
 
-	def generate_reset_token(self,expiration=3600):
-		s =Serializer(current_app.config['SECRET_KEY'],expiration)
-		return s.dumps({'reset':self.id})
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirm = True
+        self.col.update_one(
+            {'email', self.email},
+            {"$set": {"confirmed": self.confirmed}}
+        )
+        return True
 
-	def reset_password(self,token,new_password):
-		s = Serializer(current_app.config['SECRET_KEY'])
-		try:
-			data = s.loads(token)
-		except :
-			return False
-		if data.get('reset') != self.id:
-			return False
-		self.password = new_password
-		self.col.update_one(
-			{'email',self.email},
-			{"$set":{"password":self.password}}
-			)
-		return True
+    def generate_reset_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id})
 
-print User.query({'username':'joyce74'}).location
+    def reset_password(self, token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('reset') != self.id:
+            return False
+        self.password = new_password
+        self.col.update_one(
+            {'email', self.email},
+            {"$set": {"password": self.password}}
+        )
+        return True
+
+print User.query({'username': 'joyce74'}).location
