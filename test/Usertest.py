02a9814f6294f3c8a28a5127ee8f5dcea1ffa9c7
王萌 <eclipse@wangmengdeMacBook-Air.local>
2016-04-15 09:26:02 +0800

    ADD UserTest

--- test/Usertest.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('..')
-from app.main import models
-
-user = models.User(id=000001,email="eclipse_sv@163.com",
-					username="wm",password="abc@123",
-					location="whu")
-
-models.User.generate_fake(100)
---
+++ test/testUser.py
@@ -0,0 +1,30 @@
+import unittest
+import sys
+sys.path.append('..')
+from app.main import models
+
+
+class UserTestCase(unittest.TestCase):
+	def test_query(self):
+		qury={'username':'wm'}
+		self.assertEqual(models.User.query(qury).id,1)
+
+	def test_commit(self):
+		import forgery_py
+		from random import randint
+		email = forgery_py.internet.email_address()
+		username = forgery_py.internet.user_name(True)
+		data = {"id":randint(1, 10000),
+				"email":email,
+				"username":username,
+				"password":forgery_py.lorem_ipsum.word(),
+				"location":forgery_py.address.city(),
+				"about_me":forgery_py.lorem_ipsum.sentence()
+			}
+		models.User.commit(data)
+		self.assertEqual(models.User.query(data).email,email)
+
+
+
+if __name__=='__main__':
+	unittest.main()
